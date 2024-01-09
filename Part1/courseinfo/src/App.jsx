667bb87fc923165232e358306bcba7fba54ac998
arosalesfarias <arosalesfarias@gmail.com>
2024-01-09 10:04:22 -0300

    Part 1.3, Modify the variable definitions

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,17 +1,23 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exe1={exercises1} part2={part2} exe2={exercises2} part3={part3} exe3={exercises3}/>
-      <Total exe1={exercises1} exe2={exercises2} exe3={exercises3}/>   
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total part1={part1} part2={part2} part3={part3}/>   
     </div>
   )
 }
@@ -27,9 +33,9 @@ const Header = (props) =>{
 const Content = (props) =>{
   return(
     <div>
-      <Part part={props.part1} exe={props.exe1} />
-      <Part part={props.part2} exe={props.exe2} />
-      <Part part={props.part3} exe={props.exe3} />
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
     </div>
     )
 }
@@ -38,7 +44,7 @@ const Part = (props) =>{
   return(
     <>
       <p>
-        {props.part} {props.exe}
+        {props.part.name} {props.part.exercises}
       </p>
     </>
     )
@@ -47,7 +53,7 @@ const Part = (props) =>{
 const Total = (props) =>{
   return(
     <>
-      <p>Number of exercises {props.exe1 + props.exe2 + props.exe3}</p>
+      <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
     </>
     )
 }
