a9365a2250478c7945a3e9a16e000dbdf937e22b
arosalesfarias <arosalesfarias@gmail.com>
2024-01-12 01:31:51 -0300

    Part 1.8, Unicafe step 3

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [positive, setPositive] = useState(good / (good + neutral + bad) * 100)
-  const [average, setAverage] = useState((good + neutral * 0 + bad * -1)/ (good + neutral + bad))
+  const [average, setAverage] = useState((good + bad * -1)/ (good + neutral + bad))
 
   const handleOnClikGood = () => {
     setGood(good+1)
@@ -22,7 +22,7 @@ const App = () => {
   }
 
   const refreshStatisticts = (_good, _neutral, _bad) => {
-    setAverage((_good + _neutral * 0 + _bad * -1)/ (_good + _neutral + _bad))
+    setAverage((_good + _bad * -1)/ (_good + _neutral + _bad))
     setPositive(_good / (_good + _neutral + _bad) * 100)
   }
 
@@ -32,6 +32,14 @@ const App = () => {
       <button onClick={handleOnClikGood}>good</button>
       <button onClick={handleOnClikNeutral}>neutral</button>
       <button onClick={handleOnClikBad}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} average={average} positive={positive}></Statistics>
+    </div>
+  )
+}
+
+const Statistics = ({good, neutral, bad, average, positive}) => {
+  return(
+    <>
       <h1>Statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
@@ -39,7 +47,7 @@ const App = () => {
       <p>all {good + neutral + bad}</p>
       <p>average {average}</p>
       <p>positive {positive} %</p>
-    </div>
+    </>
   )
 }
 
