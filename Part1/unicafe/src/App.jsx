aa1f03b9ede767948a8ac5d16ec11ad344c51110
arosalesfarias <arosalesfarias@gmail.com>
2024-01-12 00:34:13 -0300

    Create Proyect Unicafe

---
+++ Part1/unicafe/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      Unicafe
+    </div>
+  )
+}
+
+export default App
