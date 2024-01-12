27da6b97481b830a2d8df5724ae13950a345773e
arosalesfarias <arosalesfarias@gmail.com>
2024-01-12 00:49:03 -0300

    Part 1.6, Unicafe step 1

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -8,7 +8,14 @@ const App = () => {
 
   return (
     <div>
-      Unicafe
+      <h1>Give Feedback</h1>
+      <button onClick={()=> setGood(good+1)}>good</button>
+      <button onClick={()=> setNeutral(neutral+1)}>neutral</button>
+      <button onClick={()=> setBad(bad+1)}>bad</button>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
