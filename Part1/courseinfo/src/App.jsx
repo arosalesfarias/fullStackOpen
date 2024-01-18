745e31c3bf1d1b91ad258e607e87394852804b6f
arosalesfarias <arosalesfarias@gmail.com>
2024-01-17 22:54:16 -0300

    Part 2.5, courseinfo separated module

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,31 +1,4 @@
-const Course = ({ course }) => {
-
-  const initTotal = course.parts.reduce(
-    (sum, v) => sum + v.exercises
-  , 0)
-
-  return(
-  <>
-    <Header name={course.name}></Header>
-    <Content parts={course.parts}></Content>
-    <Total sum={initTotal} />
-  </>
-  )
-}
-
-const Header = ({ name }) => <h2>{name}</h2>
-
-const Total = ({ sum }) => <p><strong>Total of {sum} exercises</strong></p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    {parts.map((part)=> <Part key={part.id} part={part}/>)}  
-  </>
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
