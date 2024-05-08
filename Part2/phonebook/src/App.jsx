a544d4c1d37e769983c3c2903e165d5717f06533
Ariel Rosales <arosalesfarias@gmail.com>
2024-05-07 22:23:03 -0300

    Part 3.19, phonebook database step 7. Display message of Validation error Mongoose post

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -3,25 +3,25 @@ import personService from './services/persons'
 import Message from './components/Message'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([]) 
-  const returnPerson = (name , number) => ({name, number})
+  const [persons, setPersons] = useState([])
+  const returnPerson = (name, number) => ({ name, number })
 
-  const [ newPerson, setNewPerson ] = useState(returnPerson('',''))
-  const [ nameFilter, setNameFilter] = useState('')
-  
-  const [ message, setMessage ] = useState({text:null, isSuccess: null})
+  const [newPerson, setNewPerson] = useState(returnPerson('', ''))
+  const [nameFilter, setNameFilter] = useState('')
+
+  const [message, setMessage] = useState({ text: null, isSuccess: null })
 
   const personsEffect = () => {
     personService.getAll()
-    .then(data => {
-      if (Array.isArray(data)) setPersons(data)
-    })
+      .then(data => {
+        if (Array.isArray(data)) setPersons(data)
+      })
   }
-  useEffect(personsEffect,[])
+  useEffect(personsEffect, [])
 
-  const changeName = event => setNewPerson(returnPerson(event.target.value , newPerson.number))
+  const changeName = event => setNewPerson(returnPerson(event.target.value, newPerson.number))
 
-  const changeNumber = event => setNewPerson(returnPerson(newPerson.name , event.target.value))
+  const changeNumber = event => setNewPerson(returnPerson(newPerson.name, event.target.value))
 
   const changeNameFilter = event => setNameFilter(event.target.value)
 
@@ -29,88 +29,91 @@ const App = () => {
 
   const addClick = event => {
     event.preventDefault()
-    persons.some(nameExists)? replaceNumber(): addNumber()
+    persons.some(nameExists) ? replaceNumber() : addNumber()
   }
 
   const addNumber = () => {
     personService.post(newPerson)
-    .then( data => {
-        setPersons(persons.concat({...newPerson, id: data.id}))
-        setNewPerson(returnPerson('','')) 
-        displayMessage(`Added ${newPerson.name}`,true)
-    })
+      .then(data => {
+        setPersons(persons.concat({ ...newPerson, id: data.id }))
+        setNewPerson(returnPerson('', ''))
+        displayMessage(`Added ${newPerson.name}`, true)
+      })
+      .catch(error => {
+        displayMessage(error.response.data.error, false)
+      })
   }
 
   const replaceNumber = () => {
     const [person] = persons.filter(nameExists)
-    if( person.number === newPerson.number){
+    if (person.number === newPerson.number) {
       alert(`${newPerson.name} is already added to phonebook`)
-    }else{
-      if(window.confirm(`${newPerson.name} is already added to phonebook, replace the old number with a new one?`) === true){
+    } else {
+      if (window.confirm(`${newPerson.name} is already added to phonebook, replace the old number with a new one?`) === true) {
         const replacePerson = { ...newPerson, id: person.id }
         personService.update(replacePerson)
-        .then(data => {
-          const newPersons = persons.filter( p => p.id !== replacePerson.id)
-          setPersons(newPersons.concat(replacePerson))
-          displayMessage(`${replacePerson.name} has replaced the number`,true)
-        })
-        .catch(error => {
-          if (error.response.status === 404)
-            displayMessage(`Information of ${replacePerson.name} has been removed from server`,false)
-          else
-            displayMessage(`${replacePerson.name} ${error.response.statusText}`,false)
+          .then(data => {
+            const newPersons = persons.filter(p => p.id !== replacePerson.id)
+            setPersons(newPersons.concat(replacePerson))
+            displayMessage(`${replacePerson.name} has replaced the number`, true)
+          })
+          .catch(error => {
+            if (error.response.status === 404)
+              displayMessage(`Information of ${replacePerson.name} has been removed from server`, false)
+            else
+              displayMessage(`${replacePerson.name} ${error.response.statusText}`, false)
 
-          setPersons(persons.filter(p => p.id !== replacePerson.id))
-        })
+            setPersons(persons.filter(p => p.id !== replacePerson.id))
+          })
       }
     }
   }
 
-  const displayMessage = (text,isSuccess) => {
-    setMessage({text,isSuccess})
-    setTimeout(()=> setMessage({text: null, isSuccess: null})
-    ,5000)
+  const displayMessage = (text, isSuccess) => {
+    setMessage({ text, isSuccess })
+    setTimeout(() => setMessage({ text: null, isSuccess: null })
+      , 5000)
   }
 
-  const personsFilter = () => 
-    persons.filter( person => person.name.toLowerCase().includes(nameFilter.toLocaleLowerCase()))
+  const personsFilter = () =>
+    persons.filter(person => person.name.toLowerCase().includes(nameFilter.toLocaleLowerCase()))
 
   const removePerson = person => {
-    if (window.confirm('Delete '+ person.name + '?' ) == true) {
+    if (window.confirm('Delete ' + person.name + '?') == true) {
       personService.remove(person)
-      .then( data => {
-        setPersons(persons.filter(
-          p => p.id !== person.id
-        ))
-      })
+        .then(data => {
+          setPersons(persons.filter(
+            p => p.id !== person.id
+          ))
+        })
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Message message={message}/>
+      <Message message={message} />
       <Filter nameFilter={nameFilter} changeNameFilter={changeNameFilter} />
       <h3>add a new</h3>
       <PersonForm newPerson={newPerson} changeName={changeName} changeNumber={changeNumber} addClick={addClick} />
       <h3>Numbers</h3>
-      <Persons persons={personsFilter()} removePerson={removePerson}/>
+      <Persons persons={personsFilter()} removePerson={removePerson} />
     </div>
   )
 }
 
-const Filter = ({ nameFilter, changeNameFilter}) =>
+const Filter = ({ nameFilter, changeNameFilter }) =>
   <div>
-    filter show with<input name='nameFilter' value={nameFilter} onChange={changeNameFilter}/>
+    filter show with<input name='nameFilter' value={nameFilter} onChange={changeNameFilter} />
   </div>
 
-const PersonForm = ({newPerson, changeName, changeNumber, addClick}) =>
+const PersonForm = ({ newPerson, changeName, changeNumber, addClick }) =>
   <form>
     <div>
-      Name: <input name='newName' value={newPerson.name} onChange={changeName}/>
+      Name: <input name='newName' value={newPerson.name} onChange={changeName} />
     </div>
     <div>
-      Number: <input name='newNumber' value={newPerson.number} onChange={changeNumber}/>
+      Number: <input name='newNumber' value={newPerson.number} onChange={changeNumber} />
     </div>
     <div>
       <button type="submit" onClick={addClick}>add</button>
@@ -119,16 +122,16 @@ const PersonForm = ({newPerson, changeName, changeNumber, addClick}) =>
 
 const Persons = ({ persons, removePerson }) =>
   <>
-    {persons.map((p)=> <Person key={p.name} person={p} removePerson={removePerson} />)}
+    {persons.map((p) => <Person key={p.name} person={p} removePerson={removePerson} />)}
   </>
 
 const Person = ({ person, removePerson }) => {
   const removeButton = () => removePerson(person)
 
-  return(
-  <>
-    <p>{person.name} {person.number} <button onClick={removeButton}>delete</button></p>
-  </>
+  return (
+    <>
+      <p>{person.name} {person.number} <button onClick={removeButton}>delete</button></p>
+    </>
   )
 }
 
